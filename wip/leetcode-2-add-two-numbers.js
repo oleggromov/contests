1663333ad7bac4e7837bc278c3508827773b269e
Oleg Gromov <mail@oleggromov.com>
2019-05-08 10:56:15 +0300

    Simplify and address code review comments

--- wip/leetcode-2-add-two-numbers.js
+++ wip/leetcode-2-add-two-numbers.js
@@ -15,37 +15,22 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    const resultHead = { val: (l1.val || 0) + (l2.val || 0) }
-    let current = resultHead
-    let next = resultHead.val
-    l1 = l1.next
-    l2 = l2.next
+    const fakeHead = {}
 
-    while (next > 0 || l1 || l2) {
-        let val = next
-        if (val > 9) {
-            val = val - 10
-            next = 1
-        } else {
-            next = 0
-        }
+    let current = fakeHead
+    let carry = 0
 
-        if (l1) {
-            next += l1.val
-            l1 = l1.next
-        }
+    while (carry > 0 || l1 || l2) {
+        let val = ((l1 && l1.val) || 0) + ((l2 && l2.val) || 0) + carry
+        carry = Math.floor(val / 10)
+        val = val % 10
 
-        if (l2) {
-            next += l2.val
-            l2 = l2.next
-        }
+        if (l1) l1 = l1.next
+        if (l2) l2 = l2.next
 
-        current.val = val
-        if (next > 0 || l1 || l2) {
-            current.next = {}
-            current = current.next
-        }
+        current.next = { val }
+        current = current.next
     }
 
-    return resultHead
-};
+    return fakeHead.next
+}
