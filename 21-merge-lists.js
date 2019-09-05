5d95c37ee5e97620e2c41b5fabac4190d8fb0e03
Oleg Gromov <mail@oleggromov.com>
2019-09-05 22:26:38 +0300

    Add final solution

--- 21-merge-lists.js
+++ 21-merge-lists.js
@@ -1,55 +1,23 @@
-function ListNode(val) {
-  this.val = val
-  this.next = null
-}
-
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-
 var mergeTwoLists = function(l1, l2) {
   const fakeHead = new ListNode(null)
   let current = fakeHead
 
-  while (l1 || l2) {
-    if ((l1 && l2 && l1.val <= l2.val) || !l2) {
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
       current.next = l1
       current = current.next
       l1 = l1.next
-    } else if ((l2 && l1 && l2.val <= l1.val) || !l1) {
+    }
+
+    if (l1 && l2.val <= l1.val) {
       current.next = l2
       current = current.next
       l2 = l2.next
     }
   }
 
+  if (l1) current.next = l1
+  if (l2) current.next = l2
+
   return fakeHead.next
 }
-
-const list1 = new ListNode(0)
-list1.next = new ListNode(2)
-list1.next.next = new ListNode(4)
-list1.next.next.next = new ListNode(5)
-list1.next.next.next.next = new ListNode(6)
-
-
-const list2 = new ListNode(1)
-list2.next = new ListNode(3)
-list2.next.next = new ListNode(4)
-list2.next.next.next = new ListNode(5)
-list2.next.next.next.next = new ListNode(6)
-
-function renderList(list) {
-  const result = []
-  let current = list
-  while (current) {
-    result.push(current.val)
-    current = current.next
-  }
-  console.log(JSON.stringify(result))
-  return result
-}
-
-renderList(mergeTwoLists(list1, list2))
