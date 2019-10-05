77dd3ee2f2108fab85ad865e38e9d47ccfdaf84d
Oleg Gromov <mail@oleggromov.com>
2019-10-05 16:59:26 +0300

    LinkedList: add indexed methods

--- data-structures/linked_list/linked_list.py
+++ data-structures/linked_list/linked_list.py
@@ -11,6 +11,17 @@ class LinkedList():
     element.next = self.head
     self.head = element
 
+  def add_at(self, index, element):
+    if index == 0:
+      self.add_first(element)
+    else:
+      prev = self.get_at(index - 1)
+      if prev:
+        prev.next = element
+        current = self.get_at(index)
+        if current:
+          element.next = current
+
   def add_last(self, element):
     if self.head:
       self.get_last().next = element
@@ -33,6 +44,22 @@ class LinkedList():
 
     return last
 
+  def get_at(self, index):
+    if not self.head:
+      return None
+
+    current = self.head
+    current_index = 0
+
+    while current.next and current_index < index:
+      current = current.next
+      current_index += 1
+
+    if current_index < index:
+      return None
+
+    return current
+
   def delete_first(self):
     if self.head:
       self.head = self.head.next
@@ -50,4 +77,10 @@ class LinkedList():
     elif self.head:
       self.head = None
 
-
+  def delete_at(self, index):
+    if index == 0:
+      self.delete_first()
+    else:
+      prev = self.get_at(index - 1)
+      if prev:
+        prev.next = self.get_at(index + 1)
--- data-structures/linked_list/test.py
+++ data-structures/linked_list/test.py
@@ -21,6 +21,16 @@ class TestLinkedList(unittest.TestCase):
     self.assertIs(self.list.get_first().value, 1)
     self.assertIs(self.list.get_last().value, 3)
 
+  def test_add_at(self):
+    self.assertIsNone(self.list.get_at(0))
+    self.list.add_at(0, ListElement(2))
+    self.assertIs(self.list.get_at(0).value, 2)
+    self.list.add_at(0, ListElement(1))
+    self.assertIs(self.list.get_at(0).value, 1)
+    self.assertIs(self.list.get_at(1).value, 2)
+    self.list.add_at(3, ListElement(4))
+    self.assertIsNone(self.list.get_at(3))
+
   def test_delete_first(self):
     self.list.add_last(ListElement(1))
     self.list.add_last(ListElement(2))
@@ -38,6 +48,28 @@ class TestLinkedList(unittest.TestCase):
     self.assertIs(self.list.get_last().value, 1)
     self.list.delete_last()
     self.assertIsNone(self.list.get_last())
+
+  def test_delete_at(self):
+    self.list.add_last(ListElement(1))
+    self.list.add_last(ListElement(2))
+    self.list.add_last(ListElement(3))
+    self.assertIs(self.list.get_at(1).value, 2)
+    self.list.delete_at(1)
+    self.assertIs(self.list.get_at(1).value, 3)
+    self.list.delete_at(1)
+    self.assertIsNone(self.list.get_at(1))
+    self.list.delete_at(0)
+    self.assertIsNone(self.list.get_at(0))
+
+  def test_get_at(self):
+    self.assertIsNone(self.list.get_at(0))
+    self.list.add_last(ListElement(1))
+    self.list.add_last(ListElement(2))
+    self.list.add_last(ListElement(3))
+    self.assertIs(self.list.get_at(0).value, 1)
+    self.assertIs(self.list.get_at(1).value, 2)
+    self.assertIs(self.list.get_at(2).value, 3)
+    self.assertIsNone(self.list.get_at(3))
 
 if __name__ == '__main__':
   unittest.main()
