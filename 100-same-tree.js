531845b888b9d0e81ffd97ffa7b298aba8c66bc4
Oleg Gromov <mail@oleggromov.com>
2019-11-07 13:30:17 +0300

    Add 100 same binary tree implementation

---
+++ 100-same-tree.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  const pathA = getPath(p)
+  const pathB = getPath(q)
+
+  if (pathA.length !== pathB.length) {
+    return false
+  }
+
+  for (let i = 0; i < pathA.length; i++) {
+    if (pathA[i] !== pathB[i]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+function getPath(root) {
+  const path = []
+
+  function traverse(root) {
+    if (!root) return
+    path.push(root.val)
+    path.push(root.left ? root.left.val : null)
+    path.push(root.right ? root.right.val : null)
+    traverse(root.left)
+    traverse(root.right)
+  }
+
+  traverse(root)
+
+  return path
+}
